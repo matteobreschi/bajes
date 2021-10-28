cdb12431b2da1b4a2994c550c0c4dd7a0ff95ce9
Matteo Breschi <matteo.breschi@ligo.org>
2021-10-28 11:43:01 +0200

    minors

--- bajes/pipe/scripts/bajes_core.py
+++ bajes/pipe/scripts/bajes_core.py
@@ -11,18 +11,18 @@ from bajes.pipe import set_logger, ensure_dir, init_sampler, parse_core_options
 sys.path.append(os.getcwd())
 
 def print_header(logger, tags, engine, nprocs, p_tag=False):
-    
+
     import numpy as np
     import pathlib
     from bajes import __version__, __path__, __githash__
-    
+
     # set printing tag
     print_tag = ''
     for ti in tags:
         print_tag = print_tag + ti
         if ti != tags[-1]:
             print_tag = print_tag + '+'
-    
+
     logger.info("> bajes, Bayesian Jenaer Software")
     logger.info("* VERSION : {}".format(__version__))
     logger.info("* PATH    : {}".format(__path__[0]))
@@ -35,7 +35,7 @@ def print_header(logger, tags, engine, nprocs, p_tag=False):
         logger.info("> Running bajes core with {} processes".format( nprocs))
 
 def init_core(opts):
-    
+
     import numpy as np
 
     # all models have distance and time_shift,
@@ -74,17 +74,21 @@ def init_core(opts):
     from bajes.pipe import get_likelihood_and_prior
     like , prior, use_gw = get_likelihood_and_prior(opts)
     opts.__dict__['use_gw'] = use_gw
-    
+
     # instance of Bayesian model
     from bajes.inf import Posterior
     post = Posterior(like=like, prior=prior)
-    
+
     return opts, post
 
 def finalize(inference, logger):
-    
-    import matplotlib
-    matplotlib.use('Agg')
+
+    try:
+        import matplotlib
+        matplotlib.use('Agg')
+    except Exception:
+        logger.warning("Unable to import matplotlib. Output plots will not be generated.")
+        pass
 
     # get posterior samples and produces plots
     logger.info("Saving posterior samples ...")
@@ -96,12 +100,12 @@ if __name__ == "__main__":
 
     # parse input arguments
     opts,args = parse_core_options()
-    
+
     # start memory tracing, if requested
     if opts.trace_memory:
         import tracemalloc
         tracemalloc.start(25)
-    
+
     # make output directory and initialize logger
     opts.outdir = os.path.abspath(opts.outdir)
     ensure_dir(opts.outdir)
@@ -110,7 +114,7 @@ if __name__ == "__main__":
         logger.debug("Using logger with debugging mode")
     else:
         logger = set_logger(outdir=opts.outdir, silence=opts.silence)
-    
+
     # print header
     print_header(logger, opts.tags, opts.engine, opts.nprocs)
 
@@ -150,4 +154,3 @@ if __name__ == "__main__":
 
     # produce posteriors
     finalize(inference, logger)
-
